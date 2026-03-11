3cecadf7ccde854d502b98ac2ffc995c2bdf1e7b
pavlos <pavlos@cytech.gr>
2026-03-11 15:58:25 +0200

    fix(ci): test release

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        System.out.println("hello2-3");
+        System.out.println("hello2-4");
         return "Hello RESTEasy";
     }
 }
